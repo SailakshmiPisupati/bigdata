13d21bd38524c43d676abbe9b2085a9a0103ffd8
Sailakshmi Pisupati <pika1@umbc.edu>
2018-03-22 16:26:55 -0400

    ssl implementation

--- server.js
+++ server.js
@@ -1,30 +1,23 @@
-var fs = require("fs")
-var express = require('express')
-var ws = require('./ws')
-var app = express()
-var http = require('http');
-var https = require('https');
+const express = require('express'),
+ ws = require('./ws'),
+ app = express(),
+ fs = require("fs"),
+ https = require("https"),
+ http = require('http'),
+ forceSsl = require('express-force-ssl');
 
-// app.all('*', function(req, res) {
-//   console.log("HTTP: " + req);
-//   return res.redirect("https://" + req.headers["host"] + req.url);
-// });
-
-// app.error(function(error, req, res, next) {
-//   return console.log("App error " + error + ", " + req.url);
-// });
-
+app.use(forceSsl);
 app.get('/', function (req, res) {
    res.sendFile(__dirname + '/index.html');
 })
 
-http.createServer(app).listen(3000, function () {
-   console.log('HTTP WebSocket example app listening on port 3000!')
-});
+const options = {
+      key: fs.readFileSync('encryption/key.pem', 'utf8'),
+      cert: fs.readFileSync('encryption/server.crt', 'utf8')
+};
 
-// https.createServer({
-//   key: fs.readFileSync('key.pem'),
-//   cert: fs.readFileSync('cert.pem')
-// }, app).listen(4000, function () {
-//    console.log('HTTPS WebSocket example app listening on port 4000!')
+const httpsServer = https.createServer(options, app).listen(3000);
+console.log('HTTPS Server listening on ', 3000);
+// http.createServer(app).listen(3000, function () {
+//    console.log('HTTP WebSocket example app listening on port 3000!')
 // });
