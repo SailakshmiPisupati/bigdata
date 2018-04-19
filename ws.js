45727415f0f42ec2dc8a30f83f795fd89719075d
Vineet Ahirkar <vinzee93@gmail.com>
2018-04-19 16:01:40 -0400

    refactor server side code

--- ws.js
+++ ws.js
@@ -27,23 +27,9 @@ const wss = new WebSocketServer({
       This only allows websocket connections from the specified origin
   */
   origin: 'https://localhost:3001',
-  server: httpsServer
+  server: httpsServer,
+  rejectUnauthorized: false
 });
-
-wss.on('connection', (ws, req) => {
-  limit(ws)
-  ws.isAlive = true;
-
-  ws.send('All glory to WebSockets!');
-
-  // const location = url.parse(req.url, true);
-  // console.log('req: ', Object.keys(req));
-  console.log('Origin: ', req.headers.origin); // Client IP
-  console.log('URL: ', req.url); // path of websocket url
-  console.log('remoteAddress: ', req.connection.remoteAddress);
-  // console.log('req.headers: ', Object.keys(req.headers));
-  console.log('Cookie: ', req.headers.cookie);
-  console.log('x-forwarded-for: ', req.headers['x-forwarded-for']);
 
   /*
       ** Authentication **
@@ -52,33 +38,57 @@ wss.on('connection', (ws, req) => {
       or req.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
   */
 
+wss.on('connection', (ws, req) => {
+  limit(ws)
+  ws.isAlive = true;
+
+  console.log('---------Connection Opened---------');
+  // const location = url.parse(req.url, true);
+  // console.log('req: ', Object.keys(req));
+  console.log('Origin: ', req.headers.origin); // Client IP
+  console.log('URL: ', req.url); // path of websocket url
+  console.log('remoteAddress: ', req.connection.remoteAddress);
+  // console.log('req.headers: ', Object.keys(req.headers));
+  console.log('Cookie: ', req.headers.cookie);
+  console.log('x-forwarded-for: ', req.headers['x-forwarded-for']);
+  console.log('------------------------------------');
+  ws.send('All glory to WebSockets!');
+
   ws.on('message', function (message) {
     console.log('Message received: %s', message)
   })
-
-  try {
-    const sendInterval = setInterval(() => {
-      if (ws.isAlive) {
-        ws.send(`${new Date()}`)
-      }
-
-    }, 1000)
-  } catch (e) {
-    /* handle error */
-    ws.isAlive = false;
-    console.log('WS send Error caught !')
-    throw e
-  }
 
   ws.on('pong', () => {
     this.isAlive = true;
   });
 
+  ws.on('close', function () {
+    console.log('Connection Closed');
+    clearInterval(sendInterval);
+  });
+
+  const sendInterval = setInterval(() => {
+    if (ws.isAlive) {
+      try {
+        ws.send(`${new Date()}`);
+      } catch (e) {
+        ws.isAlive = false;
+        console.log('WS send Error caught !', e);
+      }
+    } else {
+      console.log("Websocket not alive");
+    }
+
+  }, 1000)
+
 });
 
 const interval = setInterval(function ping() {
   wss.clients.forEach((ws) =>{
-    if (ws.isAlive === false) return ws.terminate();
+    if (ws.isAlive === false) {
+      console.log("Terminating WebSocket");
+      return ws.terminate();
+    }
 
     ws.isAlive = false;
     ws.ping(() => { });
